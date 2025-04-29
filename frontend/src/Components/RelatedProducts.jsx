0cbd92c12e635a0df8d913c6951908c6be5a7452
Shubz31 <shubham.kadam1971@gmail.com>
2025-04-29 20:10:02 +0530

    Update RelatedProducts.jsx

--- frontend/src/Components/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../Context/ShopContext'
+import { Title } from './Title'
 
 const RelatedProducts = ({category, subcategory}) => {
     const{ products } = useContext(ShopContext);
@@ -11,13 +12,15 @@ const RelatedProducts = ({category, subcategory}) => {
             productsCopy = productsCopy.filter((item) => category === item.category)
             productsCopy = productsCopy.filter((item) => subcategory === item.subcategory);
 
-            console.log(productsCopy.slice(0, 5));
+            setRelated(productsCopy.slice(0, 5));
         }
     },[products])
 
   return (
-    <div>
-      
+    <div className='my-24'>
+      <div className='text-center text-3xl py-2'>
+        
+      </div>
     </div>
   )
 }
